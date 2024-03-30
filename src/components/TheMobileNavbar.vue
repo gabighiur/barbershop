96d91eb9bd7ca027ed665535f0c2af4bf31d2b9a
Ghiur Gabi <gabi.ghiur@yahoo.com>
2024-03-30 18:35:32 +0200

    opt

--- src/components/TheMobileNavbar.vue
+++ src/components/TheMobileNavbar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { availableLocales, loadLanguageAsync } from '~/modules/i18n'
 
 const { t, locale } = useI18n()
@@ -13,24 +13,20 @@ const SERVICES = { label: t('nav.item3'), route: '/services', section: '' }
 const ABOUT = { label: t('nav.item5'), route: '/about', section: '' }
 
 const socialLinks = [
-  { name: 'GitHub', icon: 'i-carbon-logo-facebook', link: 'https://www.facebook.com/p/Gold-Man-Barbershop-100075883749894/', label: 'socials.github' },
-  { name: 'Instagram', icon: 'i-carbon-logo-instagram', link: 'https://www.instagram.com/goldman.barbershop/', label: 'socials.instagram' },
+  { name: 'GitHub', icon: 'i-carbon-logo-facebook', link: 'https://www.facebook.com/p/Gold-Man-Barbershop-100075883749894/', label: t('socials.github') },
+  { name: 'Instagram', icon: 'i-carbon-logo-instagram', link: 'https://www.instagram.com/goldman.barbershop/', label: t('socials.instagram') },
 ]
 
 async function toggleLocales() {
-  const locales = availableLocales
+  const locales = availableLocales.value
   const newLocale = locales[(locales.indexOf(locale.value) + 1) % locales.length]
   await loadLanguageAsync(newLocale)
   locale.value = newLocale
 }
 
-const nonHamburgerItems = computed(() => {
-  return []
-})
+const nonHamburgerItems = computed(() => [])
 
-const mobileMenuItems = computed(() => {
-  return [HOME, STORY, SERVICES, ABOUT]
-})
+const mobileMenuItems = computed(() => [HOME, STORY, SERVICES, ABOUT])
 
 function callPhoneNumber() {
   const phoneNumber = '+1234567890'
@@ -38,17 +34,12 @@ function callPhoneNumber() {
 }
 </script>
 
+
 <template>
-  <nav :class="{ ' z-50 sticky top-0 max-h-20 overflow-hidden': !menuOpen, 'z-50 sticky top-0 ': menuOpen }" class="flex items-center justify-between bg-teal-950 p-4">
+  <nav :class="{ 'z-50 sticky top-0 max-h-20 overflow-hidden': !menuOpen, 'z-50 sticky top-0': menuOpen }" class="flex items-center justify-between bg-teal-950 p-4">
     <!-- Non-Hamburger Menu Items -->
     <div class="gap-4 text-amber-100 md:flex">
-      <router-link
-        v-for="item in nonHamburgerItems"
-        :key="item.label"
-        :to="item.route"
-        class="px-4 py-2 text-2xl text-amber-100"
-        :class="{ 'font-bold text-cyan-950 bg-cyan-100 rounded-full': $route.path === item.route }"
-      >
+      <router-link v-for="item in nonHamburgerItems" :key="item.label" :to="item.route" class="px-4 py-2 text-2xl text-amber-100" :class="{ 'font-bold text-cyan-950 bg-cyan-100 rounded-full': $route.path === item.route }">
         {{ item.label }}
       </router-link>
     </div>
@@ -60,26 +51,21 @@ function callPhoneNumber() {
         <div class="flex flex-col items-center justify-center">
           <!-- Mobile Menu Items -->
           <div flex flex-col items-center justify-center>
-            <router-link
-              v-for="item in mobileMenuItems"
-              :key="item.label"
-              :to="item.route"
-              class="flex py-2 text-4xl text-amber-100 hover:scale-110"
-            >
+            <router-link v-for="item in mobileMenuItems" :key="item.label" :to="item.route" class="flex py-2 text-4xl text-amber-100 hover:scale-110">
               {{ item.label }}
             </router-link>
           </div>
           <!-- Button -->
           <button class="gap-2 whitespace-nowrap rounded-xl bg-amber-100 px-4 py-1 text-2xl text-teal-950 font-bold focus:bg-amber-200 hover:bg-amber-200 focus:outline-none" @click="callPhoneNumber">
             <div flex items-center gap-4>
-              <div i-carbon:phone-filled class="" />
+              <div class="i-carbon:phone-filled" />
               <span>Programeaza-te</span>
             </div>
           </button>
           <!-- Language Dropdown -->
-          <div class="py-2 text-2xl text-amber-100" @click="toggleLocales()">
+          <div class="py-2 text-2xl text-amber-100" @click="toggleLocales">
             <div flex flex-row items-center gap-4 icon-btn>
-              <div class="cursor-pointer" i-carbon-language />
+              <div class="cursor-pointer i-carbon-language" />
               <span>Change language</span>
             </div>
           </div>
@@ -89,7 +75,7 @@ function callPhoneNumber() {
               <div :class="socialLink.icon" class="text-2xl icon-btn" />
             </a>
           </div>
-          <hr w-sm border-1 border-amber-100>
+          <hr class="w-sm border-1 border-amber-100">
           <div class="mb-4 mt-20 w-30 opacity-30">
             <TheLogo />
           </div>
@@ -109,7 +95,7 @@ function callPhoneNumber() {
       <div :class="`${hamburgerLine} ${menuOpen ? '-rotate-45 -translate-y-2 opacity-100 group-hover:opacity-100 transition-transform duration-300' : 'opacity-100 group-hover:opacity-100'}`" />
     </button>
   </nav>
-  <hr border-sm mt-2 min-w-screen border-amber-100>
+  <hr class="border-sm mt-2 min-w-screen border-amber-100">
 </template>
 
 <style scoped>
